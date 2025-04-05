fe1b5925a4b18a4777b8122d9274a7e43bb3029f
onlytrialanderror <72103411+onlytrialanderror@users.noreply.github.com>
2025-04-05 19:14:14 +0200

    accept challenge fixed, as well all api-calls without body

--- ha_lichess_adapter/LichessSingleRequest.py
+++ ha_lichess_adapter/LichessSingleRequest.py
@@ -22,7 +22,8 @@ URL_TEMPLATE_CLAIM_VICTORY = "https://lichess.org/api/board/game/{}/claim-victor
 URL_TEMPLATE_DRAW = "https://lichess.org/api/board/game/{}/draw/{}"
 URL_TEMPLATE_TAKEBACK = "https://lichess.org/api/board/game/{}/takeback/{}"
 URL_TEMPLATE_CHALLENGE = "https://lichess.org/api/challenge/{}"
-
+URL_TEMPLATE_CHALLENGE_LIST = "https://lichess.org/api/challenge"
+URL_TEMPLATE_CHALLENGE_ACCEPT = "https://lichess.org/api/challenge/{}/accept"
 class LichessSingleRequest(hass.Hass):
 
     _current_game_id = IDLE_GAME_ID
@@ -41,10 +42,6 @@ class LichessSingleRequest(hass.Hass):
         self.listen_state(self.game_id_changed, LICHESS_GAME_ID_SENSOR)
         self.listen_state(self.token_changed, LICHESS_TOKEN_SENSOR)
         self.listen_state(self.handle_call_trigger, LICHESS_CALL_SENSOR)
-        self.__class__._current_header = {
-                                        "Content-Type": "application/json",
-                                        "Authorization": f"Bearer {self.__class__._current_token}"
-                                    }
         # we are ready to go
         self.log(f"Waiting for new api call")
 
@@ -60,10 +57,6 @@ class LichessSingleRequest(hass.Hass):
         if new and new != old and new != UNAVAILABLE_STATE and new != UNKNOWN_STATE:
             self.log(f"Token changed (call): {old} -> {new}")
             self.__class__._current_token = new
-            self.__class__._current_header = {
-                                        "Content-Type": "application/json",
-                                        "Authorization": f"Bearer {new}"
-                                    }
         else:
             if new is None or new == UNAVAILABLE_STATE or new == UNKNOWN_STATE: 
                 self.log("Not allowed token (call): {}".format(new))
@@ -92,78 +85,110 @@ class LichessSingleRequest(hass.Hass):
                 if (call_type == 'overwriteToken' and json_data.get('token')):
                     self.token_changed(None, None, self.__class__._current_token, json_data.get('token'), None)
                 else:
+                    if (call_type == 'acceptChallenge' and json_data.get('parameter')):
+                        valid_token   = self.__class__._current_token != EMPTY_STRING and self.__class__._current_token != UNAVAILABLE_STATE and self.__class__._current_token != UNKNOWN_STATE
+                        if valid_token:                            
+                            self.__class__._current_call_description = call_type + ": list of " + json_data.get('parameter')
+                            self.__class__._current_url = URL_TEMPLATE_CHALLENGE_LIST
+                            self.__class__._current_body = EMPTY_CALL 
+                            response = self.lichess_api_call_get()
 
-                    valid_game_id = self.__class__._current_game_id != IDLE_GAME_ID and self.__class__._current_game_id != UNAVAILABLE_STATE and self.__class__._current_game_id != UNKNOWN_STATE
-                    valid_token   = self.__class__._current_token != EMPTY_STRING and self.__class__._current_token != UNAVAILABLE_STATE and self.__class__._current_token != UNKNOWN_STATE
-                    if (valid_token):
-                        if (valid_game_id):                        
-                            # make a call without body
-                            if (call_type in {'makeMove', 'abort', 'resign', 'claim-victory'}):   
-                                self.__class__._current_call_description = call_type                                                 
-                                if (call_type == 'makeMove'):
-                                    self.__class__._current_url = URL_TEMPLATE_MOVE.format(self.__class__._current_game_id, json_data.get('move'))  
-                                    self.__class__._current_call_description += " " + json_data.get('move')                          
-                                if (call_type == 'abort'):
-                                    self.__class__._current_url = URL_TEMPLATE_ABORT.format(self.__class__._current_game_id)
-                                if (call_type == 'resign'):
-                                    self.__class__._current_url = URL_TEMPLATE_RESIGN.format(self.__class__._current_game_id)
-                                if (call_type == 'claim-victory'):
-                                    self.__class__._current_url = URL_TEMPLATE_CLAIM_VICTORY.format(self.__class__._current_game_id)                            
-                                self.__class__._current_body = EMPTY_CALL
 
-                            # handle draw / tackback offers or send offers
-                            if (json_data.get('type') in {'draw', 'takeback' } and json_data.get('parameter')):
-                                if (call_type == 'draw'):
-                                    self.__class__._current_url = URL_TEMPLATE_DRAW.format(self.__class__._current_game_id, json_data.get('parameter'))
-                                if (call_type == 'takeback'):
-                                    self.__class__._current_url = URL_TEMPLATE_TAKEBACK.format(self.__class__._current_game_id, json_data.get('parameter'))
-                                self.__class__._current_call_description = call_type + " " + json_data.get('parameter')
-                                self.__class__._current_body = EMPTY_CALL  
+                            # Load JSON data
+                            if 'application/json' in response.headers.get('Content-Type', ''):
 
-                        # handle challenge request
-                        if (call_type == 'createGame' and json_data.get('opponentname')):
+                                try: 
+                                    json_response = response.json()  # This will raise an exception if invalid
 
-                            if (json_data.get('opponentname') == 'random'): # we create a seek
+                                    curr_challenge_id = ""
+                                    # Loop through each item in the "in" list
+                                    for in_challenge in json_response['in']:
+                                        if in_challenge['challenger']['name'] == json_data.get('parameter'):
+                                            curr_challenge_id = in_challenge['id'] # we found the challenger from the board
+                                            break
 
-                                self.__class__._current_body = {      
-                                    "rated": json_data.get('rated', False),                      
-                                    "variant": "standard",
-                                    "color": json_data.get('color', "random"),
-                                    "time": json_data.get('time_m', 15),
-                                    "increment": json_data.get('increment', 0)
-                                }
-                                self.__class__._current_call_description = f"Seek new game ({json_data.get('time_m', 15)}+{json_data.get('increment', 0)})"
-                                self.__class__._current_url = URL_TEMPLATE_SEEK
+                                    if curr_challenge_id != "":
+                                        # we accept challenge with this ID
+                                        self.__class__._current_url = URL_TEMPLATE_CHALLENGE_ACCEPT.format(curr_challenge_id)
+                                        self.__class__._current_call_description = call_type + ": accept " + curr_challenge_id
+                                        # post api pody                    
+                                        self.lichess_api_call_post()       
+                                except requests.exceptions.JSONDecodeError:
+                                    self.log("Invalid json-response")
+       
+                    else:
 
-                            else: # create a challenge
+                        valid_game_id = self.__class__._current_game_id != IDLE_GAME_ID and self.__class__._current_game_id != UNAVAILABLE_STATE and self.__class__._current_game_id != UNKNOWN_STATE
+                        valid_token   = self.__class__._current_token != EMPTY_STRING and self.__class__._current_token != UNAVAILABLE_STATE and self.__class__._current_token != UNKNOWN_STATE
+                        if (valid_token):
+                            if (valid_game_id):                        
+                                # make a call without body
+                                if (call_type in {'makeMove', 'abort', 'resign', 'claim-victory'}):   
+                                    self.__class__._current_call_description = call_type                                                 
+                                    if (call_type == 'makeMove'):
+                                        self.__class__._current_url = URL_TEMPLATE_MOVE.format(self.__class__._current_game_id, json_data.get('move'))  
+                                        self.__class__._current_call_description += " " + json_data.get('move')                          
+                                    if (call_type == 'abort'):
+                                        self.__class__._current_url = URL_TEMPLATE_ABORT.format(self.__class__._current_game_id)
+                                    if (call_type == 'resign'):
+                                        self.__class__._current_url = URL_TEMPLATE_RESIGN.format(self.__class__._current_game_id)
+                                    if (call_type == 'claim-victory'):
+                                        self.__class__._current_url = URL_TEMPLATE_CLAIM_VICTORY.format(self.__class__._current_game_id)                            
+                                    self.__class__._current_body = EMPTY_CALL
 
-                                self.__class__._current_body = {                            
-                                    "variant": "standard",
-                                    "color": json_data.get('color'),
-                                    "keepAliveStream": False,
-                                    "clock": {
-                                                "limit": json_data.get('time_s'),
-                                                "increment": json_data.get('increment')
-                                                }                            
-                                }
+                                # handle draw / tackback offers or send offers
+                                if (json_data.get('type') in {'draw', 'takeback' } and json_data.get('parameter')):
+                                    if (call_type == 'draw'):
+                                        self.__class__._current_url = URL_TEMPLATE_DRAW.format(self.__class__._current_game_id, json_data.get('parameter'))
+                                    if (call_type == 'takeback'):
+                                        self.__class__._current_url = URL_TEMPLATE_TAKEBACK.format(self.__class__._current_game_id, json_data.get('parameter'))
+                                    self.__class__._current_call_description = call_type + " " + json_data.get('parameter')
+                                    self.__class__._current_body = EMPTY_CALL  
 
-                                # check if challenge to a user or AI
-                                username, level = self.parse_username_string(json_data.get('opponentname'))                        
-                                if (level == 0): # challege a user                            
-                                    self.__class__._current_body["rated"] = json_data.get('rated', False)
-                                else:
-                                    self.__class__._current_body["level"] = level
-                                    username = "ai" # we need lowercase for the url
-                                
-                                self.__class__._current_url = URL_TEMPLATE_CHALLENGE.format(username) 
-                                self.__class__._current_call_description = f"Seek new challenge with {json_data.get('opponentname')} ({json_data.get('time_s')}+{json_data.get('increment')})"
+                            # handle challenge request
+                            if (call_type == 'createGame' and json_data.get('opponentname')):
 
-                        # post api pody                    
-                        self.lichess_api_call()
+                                if (json_data.get('opponentname') == 'random'): # we create a seek
+
+                                    self.__class__._current_body = {      
+                                        "rated": json_data.get('rated', False),                      
+                                        "variant": "standard",
+                                        "color": json_data.get('color', "random"),
+                                        "time": json_data.get('time_m', 15),
+                                        "increment": json_data.get('increment', 0)
+                                    }
+                                    self.__class__._current_call_description = f"Seek new game ({json_data.get('time_m', 15)}+{json_data.get('increment', 0)})"
+                                    self.__class__._current_url = URL_TEMPLATE_SEEK
+
+                                else: # create a challenge
+
+                                    self.__class__._current_body = {                            
+                                        "variant": "standard",
+                                        "color": json_data.get('color'),
+                                        "keepAliveStream": False,
+                                        "clock": {
+                                                    "limit": json_data.get('time_s'),
+                                                    "increment": json_data.get('increment')
+                                                    }                            
+                                    }
+
+                                    # check if challenge to a user or AI
+                                    username, level = self.parse_username_string(json_data.get('opponentname'))                        
+                                    if (level == 0): # challege a user                            
+                                        self.__class__._current_body["rated"] = json_data.get('rated', False)
+                                    else:
+                                        self.__class__._current_body["level"] = level
+                                        username = "ai" # we need lowercase for the url
+                                    
+                                    self.__class__._current_url = URL_TEMPLATE_CHALLENGE.format(username) 
+                                    self.__class__._current_call_description = f"Seek new challenge with {json_data.get('opponentname')} ({json_data.get('time_s')}+{json_data.get('increment')})"
+
+                            # post api pody                    
+                            self.lichess_api_call_post()
                     
 
     # function to post the request to lichess api
-    def lichess_api_call(self):
+    def lichess_api_call_post(self):
         if (self.__class__._current_url != EMPTY_STRING):
 
             self.log(f"Starting the api call: {self.__class__._current_call_description}")            
@@ -172,12 +197,19 @@ class LichessSingleRequest(hass.Hass):
 
             try:
                 if (self.__class__._current_body == EMPTY_CALL):
+                    self.__class__._current_header = {
+                                        "Authorization": f"Bearer {self.__class__._current_token}"
+                                    }
                     response = requests.post(self.__class__._current_url, headers=self.__class__._current_header)
                 else:
+                    self.__class__._current_header = {
+                                        "Content-Type": "application/json",
+                                        "Authorization": f"Bearer {self.__class__._current_token}"
+                                    } 
                     response = requests.post(self.__class__._current_url , json=self.__class__._current_body, headers=self.__class__._current_header)
 
                 if (response.status_code == 200 or response.status_code == 201):
-                    self.log("Succsessed api call")
+                    self.log("Succsessed api post call")
                 else:
                     self.log(f"Error: {response.status_code}, Response: {response.text}")
 
@@ -191,5 +223,43 @@ class LichessSingleRequest(hass.Hass):
 
             # we are ready to go
             self.log(f"Waiting for new api call")
+
+    def lichess_api_call_get(self):
+        response = ""
+        if (self.__class__._current_url != EMPTY_STRING):
+
+            self.log(f"Starting the api call: {self.__class__._current_call_description}")            
+            self.log(f"URL: {self.__class__._current_url}")
+            self.log(f"Body: {self.__class__._current_body}")
+
+            try:
+                if (self.__class__._current_body == EMPTY_CALL):
+                    self.__class__._current_header = {
+                                        "Authorization": f"Bearer {self.__class__._current_token}"
+                                    }
+                    response = requests.get(self.__class__._current_url, headers=self.__class__._current_header)
+                else:
+                    self.__class__._current_header = {
+                                        "Content-Type": "application/json",
+                                        "Authorization": f"Bearer {self.__class__._current_token}"
+                                    } 
+                    response = requests.get(self.__class__._current_url , json=self.__class__._current_body, headers=self.__class__._current_header)  
+
+                if (response.status_code == 200):
+                    self.log("Succsessed api get call")
+                else:
+                    self.log(f"Error: {response.status_code}, Response: {response.text}")
+
+            except requests.exceptions.RequestException as e:
+                self.log(f"Error: {e}")
+            
+            # reset all values
+            self.__class__._current_url = EMPTY_STRING
+            self.__class__._current_body = EMPTY_CALL
+            self.__class__._current_call_description = EMPTY_STRING
+
+            # we are ready to go
+            self.log(f"Waiting for new api call")
+        return response
 
 
